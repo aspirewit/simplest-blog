dd0ff7cba2777bb06befa5be00b6b0f432cf67da
Richard Li <aspirewit@gmail.com>
2019-03-30 13:44:54 +0800

    Add HomeController

---
+++ src/main/java/info/chixin/simplestblog/SimplestBlogApplication.java
@@ -0,0 +1,12 @@
+package info.chixin.simplestblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplestBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimplestBlogApplication.class, args);
+    }
+}
--- src/test/java/info/chixin/simplestblog/SimplestBlogApplicationTests.java
+++ src/test/java/info/chixin/simplestblog/SimplestBlogApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.simplestblog;
+package info.chixin.simplestblog;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+public class SimplestBlogApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
